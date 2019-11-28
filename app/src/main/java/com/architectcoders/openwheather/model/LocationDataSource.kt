f3a78beeabe425d026a015b83daf22a2ffa43bb2
Cabezas <javiergonzalezcabezas@gmail.com>
2019-11-28 09:52:26 +0100

    added location

---
+++ app/src/main/java/com/architectcoders/openwheather/model/LocationDataSource.kt
@@ -0,0 +1,26 @@
+package com.architectcoders.openwheather.model
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.location.Location
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+import com.google.android.gms.location.LocationServices
+
+interface LocationDataSource {
+    suspend fun findLastLocation(): Location?
+}
+
+class PlayServicesLocationDataSource(activity: Activity) : LocationDataSource {
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+
+    @SuppressLint("MissingPermission")
+    override suspend fun findLastLocation(): Location? =
+        suspendCancellableCoroutine { continuation ->
+            fusedLocationClient.lastLocation
+                .addOnCompleteListener {
+                    continuation.resume(it.result)
+                }
+        }
+}
---
+++ app/src/main/java/com/architectcoders/openwheather/model/PermissionChecker.kt
@@ -0,0 +1,30 @@
+package com.architectcoders.openwheather.model
+
+import android.app.Activity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.BasePermissionListener
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+class PermissionChecker (private val activity: Activity, private val permission: String) {
+
+    suspend fun request(): Boolean =
+        suspendCancellableCoroutine { continuation ->
+            Dexter
+                .withActivity(activity)
+                .withPermission(permission)
+                .withListener(object : BasePermissionListener() {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                        continuation.resume(true)
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                        continuation.resume(false)
+                    }
+                }
+                ).check()
+        }
+
+}
