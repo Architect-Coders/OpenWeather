57f61ffd05661451c819b9bac6b4b13561506b34
Cabezas <javiergonzalezcabezas@gmail.com>
2019-11-30 17:57:32 +0100

    fixed api key

--- app/src/main/java/com/architectcoders/openweather/model/WeatherRepository.kt
+++ app/src/main/java/com/architectcoders/openweather/model/WeatherRepository.kt
@@ -1,22 +1,24 @@
 package com.architectcoders.openweather.model
 
 import android.app.Activity
+import com.architectcoders.openweather.R
 
-class WeatherRepository (activity: Activity) {
+class WeatherRepository(activity: Activity) {
     companion object {
         const val LAT = "51"
         const val LON = "0"
-        const val APP_ID = ""
     }
+
     //private val apiKey = activity.getString(R.string.api_key)
     private val regionRepository = RegionRepository(activity)
+    private val appID = activity.resources.getString(R.string.key_app)
 
     suspend fun findWeather() =
         WeatherDB.service
             .getLocationWeather(
                 LAT,
                 LON,
-                APP_ID
+                appID
             )
             .await()
 }
