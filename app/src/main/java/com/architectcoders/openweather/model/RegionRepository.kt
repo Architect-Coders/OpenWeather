205be4abf916bac168acbafde2860fe96f0016fb
Cabezas <javiergonzalezcabezas@gmail.com>
2019-11-30 18:24:12 +0100

    added lat and lon in api call

--- app/src/main/java/com/architectcoders/openweather/model/RegionRepository.kt
+++ app/src/main/java/com/architectcoders/openweather/model/RegionRepository.kt
@@ -7,30 +7,32 @@ import android.location.Location
 
 class RegionRepository (activity: Activity) {
 
-    companion object {
-        private const val DEFAULT_REGION = "US"
-    }
-
     private val locationDataSource = PlayServicesLocationDataSource(activity)
     private val coarsePermissionChecker = PermissionChecker(
         activity,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
-    private val geocoder = Geocoder(activity)
 
-    suspend fun findLastRegion(): String = findLastLocation().toRegion()
+    suspend fun findLat(): String = findLastLocation().toLat()
+    suspend fun findLon(): String = findLastLocation().toLon()
 
     private suspend fun findLastLocation(): Location? {
         val success = coarsePermissionChecker.request()
         return if (success) locationDataSource.findLastLocation() else null
     }
 
-    private fun Location?.toRegion(): String {
+    private fun Location?.toLat(): String {
         val addresses = this?.let {
-            geocoder.getFromLocation(latitude, longitude, 1)
+            latitude
         }
-        return addresses?.firstOrNull()?.countryCode ?: DEFAULT_REGION
+        return "$addresses".substring(0..1)
     }
 
+    private fun Location?.toLon(): String {
+        val addresses = this?.let {
+            longitude
+        }
+        return "$addresses".substring(0..1)
+    }
 
 }
--- app/src/main/java/com/architectcoders/openweather/model/WeatherRepository.kt
+++ app/src/main/java/com/architectcoders/openweather/model/WeatherRepository.kt
@@ -4,20 +4,15 @@ import android.app.Activity
 import com.architectcoders.openweather.R
 
 class WeatherRepository(activity: Activity) {
-    companion object {
-        const val LAT = "51"
-        const val LON = "0"
-    }
 
-    //private val apiKey = activity.getString(R.string.api_key)
     private val regionRepository = RegionRepository(activity)
     private val appID = activity.resources.getString(R.string.key_app)
 
     suspend fun findWeather() =
         WeatherDB.service
             .getLocationWeather(
-                LAT,
-                LON,
+                regionRepository.findLat(),
+                regionRepository.findLon(),
                 appID
             )
             .await()
