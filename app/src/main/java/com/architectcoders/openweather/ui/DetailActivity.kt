a5ff9097533b46182df98a0373f50e2fcd200e48
Cabezas <javiergonzalezcabezas@gmail.com>
2019-12-01 22:14:26 +0100

    added image in detail

--- app/src/main/java/com/architectcoders/openweather/ui/DetailActivity.kt
+++ app/src/main/java/com/architectcoders/openweather/ui/DetailActivity.kt
@@ -4,21 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.architectcoders.openweather.R
 import com.architectcoders.openweather.model.detail.Detail
+import com.architectcoders.openweather.model.image.CreateImage
 import kotlinx.android.synthetic.main.detail_activity.*
 
 class DetailActivity : AppCompatActivity() {
     companion object {
         const val WEATHER = "DetailActivity:weather"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_activity)
 
         with(intent.getParcelableExtra<Detail>(WEATHER)) {
             movieDetailToolbar.title = city
-            movieDetailImage.setImageResource(R.drawable.ic_mist)
+            showImage(main)
             movieDetailSummary.text = main
             movieDetailInfo.setMovie(this)
         }
+    }
+
+    private fun showImage(image: String) {
+        movieDetailImage.setImageDrawable(
+            CreateImage().getImageFromString(
+                image,
+                this
+            )
+        )
     }
 }
